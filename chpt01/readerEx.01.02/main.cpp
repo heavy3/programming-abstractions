d94ca8828ef4ff1012a867ea8e5904bbbdd13cb8
zenglenn42 <gsnospampls@pobox.com>
2017-02-02 14:41:55 -0600

    re-add exercise 2 & 3

---
+++ chpt01/readerEx.01.02/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//
+//  This program converts distances provided in meters to distances
+//  in feet and inches using the following conversion factors:
+//
+//  1 inch = 0.0254 meters 
+//  1 foot = 12 inches
+//
+//  --------------------------------------------------------------------------
+//  Attribution: "Programming Abstractions in C++" by Eric Roberts
+//  Chapter 1, Exercise 2
+//  Stanford University, Autumn Quarter 2012
+//  http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/materials/CS106BX-Reader.pdf
+//  --------------------------------------------------------------------------
+//
+//  Created by Glenn Streiff on 9/12/15.
+//  Copyright © 2015 Glenn Streiff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    const double meterPerInch = 0.0254;
+    const double inchPerFoot  = 12.0;
+    
+    double totalMeters;
+    
+    std::cout << "Enter a distance in meters that i'll convert to feet and inches." << std::endl;
+    std::cout << "Meters: ";
+    std::cin  >> totalMeters;
+    
+    double totalInches = totalMeters / meterPerInch;
+    double totalFeet   = totalInches / inchPerFoot;
+    
+    int fracFeet = int(totalFeet);
+    double fracInches = (totalFeet - fracFeet) * double(inchPerFoot);
+    
+    std::cout << "Equals: " << fracFeet << "' " << fracInches << "\"" << std::endl;
+    
+    return 0;
+}
---
+++ chpt01/readerEx.01.03/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//
+//  This program calculates the sum of numbers from 1 to 100 using
+//  using two different methods of O(n) and O(1) time complexity, 
+//  respectively.
+//
+//  --------------------------------------------------------------------------
+//  Attribution: "Programming Abstractions in C++" by Eric Roberts
+//  Chapter 1, Exercise 3
+//  Stanford University, Autumn Quarter 2012
+//  http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/materials/CS106BX-Reader.pdf
+//  --------------------------------------------------------------------------
+//
+//  Created by Glenn Streiff on 9/12/15.
+//  Copyright © 2015 Glenn Streiff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    // Iterative method, O(n) time complexity.
+
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+        sum += i;
+    }
+    std::cout << "Sum of numbers from 1 to 100 = " << sum << std::endl;
+
+    // Gauss' method, O(1) time complexity.
+    // 1 + 100
+    // 2 +  99
+    // 3 +  98
+    // ..
+    // 50 + 51
+    // 101 * 50
+    
+    sum = (100 + 1) * (100/2);
+    std::cout << "Elegant sum of numbers from 1 to 100 = " << sum << std::endl;
+    return 0;
+}
