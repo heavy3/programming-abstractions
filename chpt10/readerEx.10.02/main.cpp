731b3ff9df3362413409e9e92ae2a8027312b189
zenglenn42 <gsnospampls@pobox.com>
2017-02-06 18:15:44 -0600

    standardize comments & boilerplate

--- chpt10/readerEx.10.02/main.cpp
+++ chpt10/readerEx.10.02/main.cpp
@@ -1,12 +1,7 @@
 //
-//  main.cpp
-//  readerEx.10.02
+// main.cpp
 //
-//  Created by g on 04/27/2016.
-//  Copyright © 2016 g. All rights reserved.
-//
-//  Description
-//
+// This program implements an insertion sort algorithm.
 //  Write an implementation of sort that uses the insertion sort algorithm.
 //
 //  Construct an informal argument to show that the worst-case behavior of
@@ -17,14 +12,16 @@
 //  It therefore makes sense to use insertion sort to restore order to a large
 //  vector in which only a few elements are out of sequence.
 //
-//  References
+// --------------------------------------------------------------------------
+// Attribution: "Programming Abstractions in C++" by Eric Roberts
+// Chapter 10, Exercise 2
+// Stanford University, Autumn Quarter 2012
+// http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/materials/CS106BX-Reader.pdf
+// --------------------------------------------------------------------------
 //
-//  Eric Robert's CS106B C++ Class Reader:
+// Created by Glenn Streiff on 4/27/16.
+// Copyright © 2016 Glenn Streiff. All rights reserved.
 //
-//  Chapter 10
-//  Programming Abstractions in C++
-//  Stanford University, Autumn Quarter 2012
-//  http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/materials/CS106BX-Reader.pdf
 
 #include <iostream>
 #include <iomanip>
@@ -34,38 +31,40 @@ using namespace std;
 
 #define HISTOGRAM   // Comment this out to disable histogram output.
 
-/* Constants */
+// Constants
 
 const std::string HEADER = "CS106B Programming Abstractions in C++: Ex 10.02\n";
 const std::string DETAIL = "Insertion Sort -- worst case O(N^2)";
 const std::string BANNER = HEADER + DETAIL;
 
-/* Prototypes */
+// Prototypes
 
-void sort(Vector<int> & nums);                      /* Insertion sort */
+void sort(Vector<int> & nums);  // Insertion sort
 std::string toString(Vector<int> & nums, int s);
 
-/* Main program */
+// Main program
 
 int main() {
     cout << BANNER << endl << endl;
     Vector<Vector<int> > testNums;
     Vector<int> nums;
     
-    /* Example vector in book. */
+    // Example vector in book.
+
     nums += 56, 25, 37, 58, 95, 19, 73, 30;
     testNums.add(nums);
     
-    /* 
-     * Worst case vector: Input data in reverse order. 
-     * Complexity = O(n^2), where n = size of input vector.
-     */
+    // 
+    // Worst case vector: Input data in reverse order. 
+    // Complexity = O(n^2), where n = size of input vector.
+    //
 
     nums.clear();
     nums += 7, 6, 5, 4, 3, 2, 1;
     testNums.add(nums);
     
-    /* Best case vector; already sorted. O(n) complexity. See histogram. */
+    // Best case vector; already sorted. O(n) complexity. See histogram.
+
     nums.clear();
     nums += 1, 2, 3, 4, 5, 6, 7;
     testNums.add(nums);
@@ -79,75 +78,75 @@ int main() {
     return 0;
 }
 
-/*
- * Function: sort
- * Usage: sort(intNumVector);
- * --------------------------
- * Sorts a vector of integers that have been passed in by reference.
- *
- * Uses an Insertion Sort algorithm that iterates over the the vector
- * gradually mutating the contents into ascending order as follows:
- *
- *
- *  [ 56 | 25 | 37 | 58 | 95 | 19 | 73 | 30 ]
- *    --
- *      \_ in order
- *
- *  [ 25 | 56 | 37 | 58 | 95 | 19 | 73 | 30 ]
- *    --   --
- *           \_ in order
- *
- *  [ 25 | 37 | 56 | 58 | 95 | 19 | 73 | 30 ]
- *    --   --   --
- *                \_ in order
- *
- * Worst-case performance is O(N^2) when input is reversed sorted.
- * Base-case performance is O(N) when input is already sorted.
- *
- * This algorithm is suited to input that is partially sorted.
- *
- * Complexity Analysis
- * -------------------
- * Broadly speaking, we have a double loop structure with variable cost
- * contributed from inner loop comparisons plus a repositioning
- * vector mutation.
- *
- * Roughly, that gives a runtime of:
- *
- *  t(n) ~= (n-1) * (t_innerloop + t_mutation)
- *
- * t_innerloop  effort varies from 1 to (n-1) iterations depending upon
- *              structure of input data.
- *
- *              Worst case is repositioning final unsorted number from
- *              one end of the vector to the other (especially when data
- *              are in reversed order initially).
- *
- * t_mutation   effort to reposition a single number within the vector
- *              depends upon the underlying implementation of the data type.
- *
- *              In the best case, this would be constant time.
- *              In the worst case, it would be at most 2*(n-1) operations
- *              to remove an unsorted number and re-insert it
- *              in sorted order (especially at the beginning of the sorting
- *              activity).
- *
- * Assuming worst case for both inner loop actions, we get:
- *
- *  t(n) ~= (n-1) * (c2(n-1) + 2(n-1))
- *
- * Simplifying:
- *
- *  t(n) ~= (n-1) * (3n -1) = 3n^2 - 4n + 1 + c0
- *
- * Dropping non-dominant lower-order terms and constants, we get:
- *
- *  O(n^2)
- *
- * Interestingly, the qualitative analysis yields the same result even if
- * the vector mutation happens in constant time, since the inner loop
- * still dominates with an O(n) term.
- */
+//
+// Function: sort
+// Usage: sort(intNumVector);
+// --------------------------
+// Sorts a vector of integers that have been passed in by reference.
+//
+// Uses an Insertion Sort algorithm that iterates over the the vector
+// gradually mutating the contents into ascending order as follows:
+//
+//
+//  [ 56 | 25 | 37 | 58 | 95 | 19 | 73 | 30 ]
+//    --
+//      \_ in order
+//
+//  [ 25 | 56 | 37 | 58 | 95 | 19 | 73 | 30 ]
+//    --   --
+//           \_ in order
+//
+//  [ 25 | 37 | 56 | 58 | 95 | 19 | 73 | 30 ]
+//    --   --   --
+//                \_ in order
+//
+// Worst-case performance is O(N^2) when input is reversed sorted.
+// Base-case performance is O(N) when input is already sorted.
+//
+// This algorithm is suited to input that is partially sorted.
+//
+// Complexity Analysis
+// -------------------
+// Broadly speaking, we have a double loop structure with variable cost
+// contributed from inner loop comparisons plus a repositioning
+// vector mutation.
+//
+// Roughly, that gives a runtime of:
+//
+//  t(n) ~= (n-1) * (t_innerloop + t_mutation)
+//
+// t_innerloop  effort varies from 1 to (n-1) iterations depending upon
+//              structure of input data.
+//
+//              Worst case is repositioning final unsorted number from
+//              one end of the vector to the other (especially when data
+//              are in reversed order initially).
+//
+// t_mutation   effort to reposition a single number within the vector
+//              depends upon the underlying implementation of the data type.
+//
+//              In the best case, this would be constant time.
+//              In the worst case, it would be at most 2*(n-1) operations
+//              to remove an unsorted number and re-insert it
+//              in sorted order (especially at the beginning of the sorting
+//              activity).
+//
+// Assuming worst case for both inner loop actions, we get:
+//
+//  t(n) ~= (n-1) * (c2(n-1) + 2(n-1))
+//
+// Simplifying:
+//
+//  t(n) ~= (n-1) * (3n -1) = 3n^2 - 4n + 1 + c0
+//
+// Dropping non-dominant lower-order terms and constants, we get:
+//
+//  O(n^2)
+//
+// Interestingly, the qualitative analysis yields the same result even if
+// the vector mutation happens in constant time, since the inner loop
+// still dominates with an O(n) term.
+//
 
 void sort(Vector<int> & items) {
     for (int u = 1; u < items.size(); u++) {
@@ -189,13 +188,13 @@ void sortUgly(Vector<int> & nums) {
     }
 }
 
-/*
- * Function: toString
- * Usage: cout << toString(numVector, partionIndex);
- * -------------------------------------------------
- * Display a vector of integers with a vertical parition separating the
- * sorted portion of the vector from the unsorted.
- */
+//
+// Function: toString
+// Usage: cout << toString(numVector, partionIndex);
+// -------------------------------------------------
+// Display a vector of integers with a vertical parition separating the
+// sorted portion of the vector from the unsorted.
+//
 
 std::string toString(Vector<int> & nums, int s) {
     ostringstream oss;
