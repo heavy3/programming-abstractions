db589a57a48d4bdda3478f831d9610334812bd2f
zenglenn42 <gsnospampls@pobox.com>
2017-02-17 10:31:27 -0600

    commit solutions for chapter 4 (ex. 4)

---
+++ chpt04/readerEx.04.04/main.cpp
@@ -0,0 +1,89 @@
+//
+// main.cpp
+//
+// This program counts the number characters, words, and lines
+// in a file.
+//
+// For example:
+//
+//    CS106B Programming Abstractions in C++: Ex 4.04
+//    Count characters, words, and lines in a file.
+//
+//    Input file: Lear.txt
+//    Chars: 254
+//    Words:  43
+//    Lines:   6
+//
+// --------------------------------------------------------------------------
+// Attribution: "Programming Abstractions in C++" by Eric Roberts
+// Chapter 4, Exercise 4
+// Stanford University, Autumn Quarter 2012
+// http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/materials/CS106BX-Reader.pdf
+// --------------------------------------------------------------------------
+//
+// Created by Glenn Streiff on 2/17/17.
+// Copyright © 2017 Glenn Streiff. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include "error.h"
+using namespace std;
+
+// Constants and Types
+
+const string HEADER = "CS106B Programming Abstractions in C++: Ex 4.04\n";
+const string DETAIL = "Count characters, words, and lines in a file.";
+const string BANNER = HEADER + DETAIL;
+const string PROMPT = "Input file: ";
+const string E_NO_FILE = "File not found.  Please try again.";
+
+// Function prototypes
+
+string promptForFile(ifstream & stream, string prompt);
+
+
+// Main program
+
+int main(int argc, char * argv[]) {
+	cout << BANNER << endl << endl;
+	ifstream istream;
+
+	string fname = promptForFile(istream, PROMPT);
+
+	int lineCount = 0;
+	int wordCount = 0;
+	int charCount = 0;
+
+	string currentLine;
+	while (getline(istream, currentLine)) {
+		++lineCount;
+        // Add +1 for truncated newline character.
+        charCount += currentLine.length() + 1;
+		istringstream lineStream(currentLine);
+		string word;
+		while (lineStream >> skipws >> word) { ++wordCount; }
+	}
+	istream.close();
+    
+    cout << "Chars:" << right << setw(4) << charCount << endl;
+    cout << "Words:" << right << setw(4) << wordCount << endl;
+    cout << "Lines:" << right << setw(4) << lineCount << endl;
+	return 0;
+}
+
+// Function definitions
+
+string promptForFile(ifstream & stream, string prompt) {
+	while (true) {
+		string fname;
+		cout << prompt;
+		getline(cin, fname);
+		stream.open(fname.c_str());
+		if (!stream.fail()) return fname;
+		stream.clear();
+		cerr << E_NO_FILE << endl;
+	}
+}
