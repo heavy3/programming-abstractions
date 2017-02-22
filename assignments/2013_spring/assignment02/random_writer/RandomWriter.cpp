e0cb66a27f7dc84126006e85725f3ac8b65252b3
zenglenn42 <gsnospampls@pobox.com>
2017-02-22 14:47:37 -0600

    solution for assignment 2 (Spring 2013 section)

---
+++ assignments/2013_spring/assignment02/random_writer/RandomWriter.cpp
@@ -0,0 +1,205 @@
+//
+// RandomWriter.cpp
+//
+// For a given language, certain character sequences are more likely than
+// others.  In english, for example, the letter 'q' is often followed by 'u'
+// as in the word, quick.
+//
+// A useful abstraction for capturing the sequence frequency of a given
+// input text is the Markov model.
+//
+// Building such a model allows us to start with a seed string and then
+// rely upon the model to supply the next most frequently occuring character
+// to append to the output sequence.  The new output is then fed back into
+// the model to generate subsequent output until some specified length of
+// desired content is reached.
+//
+// Also at play is the notion of Markov 'order' which specifies how much
+// sequence history should be factored in when predicting the most likely
+// next character.  An order-1 model predicts the next character based upon
+// only a single character sequence, whereas an order-2 model would look at
+// all two-character sequences on the input text to predict the likeliest
+// next character.
+//
+// For example:
+//
+//    Enter the source text: Hamlet.txt
+//    Enter the Markov order [1-10]: 10
+//                    [March within.]
+//    What thou hast done,- must send thee hence
+//    And how, and who, what means, and where 'tis fine,
+//    Make choice of whom your wisest friends
+//    And curd, like eager droppings into milk,
+//
+// --------------------------------------------------------------------------
+// Assignment: 2, Part 2 Random Writer
+// Course: CS106B "Programming Abstractions in C++"
+// Provider: Stanford University (SCPD)
+// Instructor: Keith Schwarz
+// Date: Spring 2013
+// http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/handouts/050%20Assignment%202.pdf
+// --------------------------------------------------------------------------
+//
+// Created by Glenn Streiff on 11/23/2015.
+// Copyright © 2015 Glenn Streiff. All rights reserved.
+//
+
+#include <iostream>
+#include "filelib.h"
+#include "simpio.h"
+#include "map.h"
+#include "vector.h"
+#include "error.h"
+#include "random.h"
+using namespace std;
+
+// Constants and types
+
+const string PROMPT_INPUT_TEXT = "Enter the source text: ";
+const string PROMPT_MARKOV_ORDER = "Enter the Markov order [1-10]: ";
+const string E_BAD_MARKOV_ORDER = "That value is out of range.";
+const int MIN_MARKOV_ORDER = 1;
+const int MAX_MARKOV_ORDER = 10;
+const int DFLT_NUM_CHARS = 2000;
+
+const string PROCESSING = "Processing file...";
+typedef Map<string,Vector<char> > MarkovModel_T;
+
+// Function prototypes
+
+void getMarkovInput(string & filename, ifstream & ifs, int & markovOrder);
+string getMarkovSeed(MarkovModel_T & model);
+void buildMarkovModel(ifstream & ifs, int markovOrder, MarkovModel_T & model);
+void markovWriter(MarkovModel_T model, string seed, int nChar = DFLT_NUM_CHARS);
+
+int main(int argc, char ** argv) {
+    string filename;
+    ifstream ifs;
+    int markovOrder;
+    MarkovModel_T model;
+    
+    getMarkovInput(filename, ifs, markovOrder);
+    buildMarkovModel(ifs, markovOrder, model);
+    string seed = getMarkovSeed(model);
+    markovWriter(model, seed);
+    return 0;
+}
+
+//
+// Function: getMarkovInput
+// Usage: getMarkovInput(filename, ifs, markovOrder);
+// --------------------------------------------------
+// Prompts the user for an input filename containing text to analyze
+// along with the desired Markov order.  Requires user to enter an
+// order which is within a min and max range specified by the
+// integer constants:
+//
+//    MIN_MARKOV_ORDER
+//    MAX_MARKOV_ORDER
+//
+// Attempts to open the file and associate it with an input stream which
+// is returned as a reference variable to the caller along with the filename
+// and Markov order.
+//
+
+void getMarkovInput(string & filename, ifstream & ifs, int & markovOrder) {
+    filename = promptUserForFile(ifs, PROMPT_INPUT_TEXT);
+    markovOrder = 0;
+    
+    while (true) {
+        markovOrder = getInteger(PROMPT_MARKOV_ORDER);
+        if ((markovOrder < MIN_MARKOV_ORDER) ||
+            (markovOrder > MAX_MARKOV_ORDER)) {
+            cout << E_BAD_MARKOV_ORDER << endl;
+        } else {
+            break;
+        }
+    }
+}
+
+string getMarkovSeed(MarkovModel_T & model) {
+    int size = 0;
+    string seed;
+    
+    //
+    // Return most frequently occurring seed.
+    // This will be the one associated with the longest vector of characters.
+    //
+
+    for (string word : model) {
+        if (model[word].size() > size) {
+            seed = word;
+            size = model[word].size();
+        }
+    }
+    return seed;
+}
+
+//
+// Function: buildMarkovModel
+// Usage: model = buildMarkovModel(ifs, order);
+// --------------------------------------------
+// Parses source text from the input file stream, building up a Markov model
+// of specified order that associates a string-snippet of fixed length (equal
+// to the order of the model) to a Vector of characters most likely to follow
+// that snippet based upon frequency analysis of the input text.
+//
+
+void buildMarkovModel(ifstream & ifs, int markovOrder, MarkovModel_T & model) {
+    if (markovOrder < 1) return;
+    
+    char ch;
+    string key;
+    while (ifs.get(ch)) {
+        if (key.length() == markovOrder) {
+            model[key].add(ch);
+            key = key.substr(1) + ch;   // Advance to next key string.
+        } else {
+            key += ch;                  // Build up first key string.
+        }
+    }
+}
+
+//
+// Function: markovWriter
+// Usage: markovWriter(model, seed);
+//        markovWriter(model, seed, numChars);
+// -------------------------------------------
+// Writes randomly generate text to the console based upon a given
+// Markov model of some source text, an input seed string, and an
+// optional text length given in characters.
+//
+// If text length is not provided, the writer defaults to the value
+// specified in the constant:
+//
+//    DFLT_NUM_CHARS
+//
+// The seed string should be a key in the Markov map, otherwise
+// no output will be produced since subsequent characters to output
+// cannot be derived from the model.
+//
+
+void markovWriter(MarkovModel_T model, string seed, int textLength) {
+    Vector<char> nextCharList;
+    char nextChar;
+    string currSeed = seed;
+    
+    if (model.size()) {
+        cout << currSeed;
+        for (int i = 0; i < textLength; i++) {
+            if (model.containsKey(currSeed)) {
+                nextCharList = model[currSeed];
+                int len = nextCharList.size();
+                if (len > 0) {
+                    nextChar = nextCharList[randomInteger(0, len - 1)];
+                    cout << nextChar;
+                    currSeed = currSeed.substr(1) + nextChar;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+}
---
+++ assignments/2013_spring/assignment02/word_ladder/WordLadder.cpp
@@ -0,0 +1,328 @@
+//
+// WordLadder.cpp
+//
+// This program prompts for two words and then attempts to construct a word
+// ladder that connects the two words via a sequence of intermediary words
+// that differ only by one letter from the adjacent words.
+//
+// Given "code" and "data" returns: code → core → care → dare → date → data
+//
+// For example:
+//
+//     Enter start word (RETURN to quit): awake
+//     Enter destination word: sleep
+//     Found ladder: awake aware sware share sharn shawn shewn sheen sheep sleep
+//     Enter start word (RETURN to quit): airplane
+//     Enter destination word: tricycle
+//     No ladder found.
+//     Enter start word (RETURN to quit):
+//
+// --------------------------------------------------------------------------
+// Assignment: 2, Part 1 Word Ladders
+// Course: CS106B "Programming Abstractions in C++"
+// Provider: Stanford University (SCPD)
+// Instructor: Keith Schwarz
+// Date: Spring 2013
+// http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1136/handouts/050%20Assignment%202.pdf
+// --------------------------------------------------------------------------
+//
+// Created by Glenn Streiff on 11/23/2015.
+// Copyright © 2015 Glenn Streiff. All rights reserved.
+//
+
+#include <iostream>
+#include "simpio.h"
+#include "strlib.h"
+#include "error.h"
+#include "tokenscanner.h"
+#include "queue.h"
+#include "vector.h"
+#include "lexicon.h"
+
+using namespace std;
+
+const string PROMPT_FIRST = "Enter start word (RETURN to quit): ";
+const string PROMPT_FINAL = "Enter destination word: ";
+const string REPORT_FOUND = "Found ladder: ";
+const string REPORT_NOT_FOUND = "No ladder found.";
+const int ndiff = -1; // Use this when difference between two strings is undef.
+
+/* Function prototypes */
+
+void getNearbyWords(int numLetterMutations,
+                    string refWord,
+                    Lexicon & allWords,
+                    Lexicon & usedWords,
+                    Lexicon & nearbyWords);
+void getNLengthWords(int length, Lexicon & allWords, Lexicon & lengthWords);
+string getWord(string prompt);
+bool getWords(string & start, string & dest);
+Vector<string> getWordLadder(string firstWord,
+                             string finalWord,
+                             Lexicon & allWords);
+void reportWordLadder(Vector<string> ladder);
+int operator -(string s1, string s2);
+
+int main() {
+    string firstWord;
+    string finalWord;
+    Lexicon allWords("EnglishWords.dat");
+    
+    while (getWords(firstWord, finalWord)) {
+        Vector<string> ladder;
+        
+        ladder = getWordLadder(firstWord, finalWord, allWords);
+        reportWordLadder(ladder);
+    }
+    return 0;
+}
+
+//
+// Function: reportWordLadder
+// Usage: reportWordLadder(ladder);
+// --------------------------------
+// Displays the results of a word ladder search to the console.
+//
+// Assumes two string constants, REPORT_FOUND and REPORT_NOT_FOUND
+// are defined with suitable text for giving context to the list of
+// words presented.
+//
+// Example: Found ladder:  work fork form foam flam flay play
+// Example: No ladder found.
+//
+
+void reportWordLadder(Vector<string> wordLadder) {
+    if (wordLadder.size()) {
+        cout << REPORT_FOUND;
+        for (string word: wordLadder) {
+            cout << " " << word;
+        }
+        cout << endl;
+    } else {
+        cout << REPORT_NOT_FOUND << endl;
+    }
+}
+
+//
+// Function: getWordLadder
+// Usage: Vector<string> ladder = getWordLadder("work", "play", lexicon);
+// ----------------------------------------------------------------------
+// Returns a Vector of strings linking the first and final words
+// through a sequence of words that differ from adjacent words
+// by 1 letter position.
+//
+// If no such sequence can be devised, an empty Vector<string> is returned.
+//
+// Example:  firstWord = "work";
+//           finalWord = "play";
+//           ladder = {"work", "fork", "form", "foam", "flam", "flay", "play"}
+//                              -          -      -      -         -    -
+//
+
+Vector<string> getWordLadder(string firstWord, string finalWord,
+                             Lexicon & allWords) {
+    //
+    // Reduce the size of the lexicon to the subset of words with the same
+    // length as the first word.
+    //
+    // This is more an intuitive stab at performance than a rigorus analysis
+    // of lexicon lookups.  Still this removes 80 - 90 percent of the words
+    // in the lexicon so there is probably some virtue here especially for
+    // longer solution lists.
+    //
+    // Also, avoid reuse of words in downstream lists.
+    //
+    
+    Lexicon nLengthWords;
+    getNLengthWords(firstWord.length(), allWords, nLengthWords);
+    
+    Lexicon usedWords;
+    usedWords.add(firstWord);
+    
+    Vector<string> noLadder;
+    Vector<string> seed;
+    seed.add(firstWord);
+    
+    if ((firstWord - finalWord) == 0) { // Overloading subtraction operator.
+        
+        // Short-circuit return if first and final are the same word.
+        
+        return seed;
+    }
+    
+    if (firstWord.length() != finalWord.length()) {
+        return noLadder;
+    }
+    
+    Queue<Vector<string> > wordLadders;
+    wordLadders.enqueue(seed);
+    
+    while (wordLadders.size()) {
+        
+        Vector<string> ladder = wordLadders.dequeue();
+        
+        //
+        // For the word list currently dequeued, inspect the last word
+        // and find the set of (unused) words that differ from it
+        // in only one letter position.
+        //
+        
+        string latestWord = ladder[ladder.size() - 1];
+        int distance = 1;
+        Lexicon nearbyWords;
+        
+        getNearbyWords(distance, latestWord, nLengthWords, usedWords,
+                       nearbyWords);
+        
+        //
+        // Create the next generation of word ladders from the current
+        // ladder by replicating it and appending one new proximate word.
+        //
+        // Check if the latest proximate word matches the desired final
+        // word.  In that case the candiate solution /is/ a solution.
+        //
+        // Eventually we'll either drain the queue dry because we run out of
+        // unused proximate words with no solution or we'll find a solution.
+        //
+        
+        for (string word : nearbyWords) {
+            Vector<string> nextLadder = ladder;
+            nextLadder.add(word);
+            if ((word - finalWord) == 0) {
+                return nextLadder;  // <<- Found solution
+            }
+            wordLadders.enqueue(nextLadder);
+        }
+    }
+    return noLadder;
+}
+
+//
+// Function: getWords
+// Usage: getWords(first, final);
+// ------------------------------
+// Returns true if the user enters two words at the console.
+// The words are returned as reference variables.
+//
+// Assumes two string constants, PROMPT_FIRST and PROMPT_FINAL
+// are defined with the text for prompting input.
+//
+
+bool getWords(string & first, string & final) {
+    first = getWord(PROMPT_FIRST);
+    if (first == "") return false;
+    
+    final = getWord(PROMPT_FINAL);
+    if (final == "") return false;
+
+    return true;
+}
+
+//
+// Function: getWord
+// Usage: string word = getWord("Enter start word (RETURN to quit): ");
+// --------------------------------------------------------------------
+// Prompts the user to enter a word which is normalized to lowercase
+// with any preceeding or trailing whitespace stripped away.
+//
+
+string getWord(string prompt) {
+    cout << prompt;
+    
+    string line = toLowerCase(getLine());
+    TokenScanner ts(line);
+    ts.ignoreWhitespace();
+    
+    string token;
+    string word = "";
+    
+    if (ts.hasMoreTokens()) {
+        token = ts.nextToken();
+        if (ts.getTokenType(token) == WORD) word = token;
+    }
+    return word;
+}
+
+//
+// Function: operator-
+// Usage: int numDifferentLetters = str1 - str2;
+// ---------------------------------------------
+// "Subtracts" two strings of the same length yielding the number of differing
+// letters.  I know, I'm being a maverick with this overload.
+//
+// Example:
+//
+// "pie" - "pay" = 2
+//
+// Strings of differing length are not valid for comparison and return
+// the constant ndiff.
+//
+// Identical strings return 0.
+//
+
+int operator-(string s1, string s2) {
+    
+    // Two words must have the same length to have a computable difference
+    // in this universe. :-)
+    
+    if (s1.length() != s2.length()) {
+        return ndiff;
+    }
+    
+    // Start by assuming the strings differ in all letter positions.
+    
+    int difference = s1.length();
+    
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] == s2[i]) {
+            difference--;       // A difference of 0 means identical.
+        }
+    }
+    return difference;
+}
+
+//
+// Function: getNearbyWords
+// Usage: getNearbyWords(1, "pie", lexicon, usedWords, nearbyWords);
+// -----------------------------------------------------------------
+// Returns a lexicon of words that are lexigraphically distant from
+// a reference word by the specified number letter differences.
+//
+// Example: The word "pie" is lexigraphically distant from
+//          the word "pin" by 1 unit since they differ in only one letter.
+//
+// Exclude from consideration any words already in the used-words
+// lexicon.  Furthermore, and new proximate words discovered are also added
+// to the used list to allow for blacklisting by the caller.
+//
+
+void getNearbyWords(int distance, string refWord, Lexicon & allWords,
+                   Lexicon & usedWords, Lexicon & nearbyWords) {
+    
+    nearbyWords.clear();
+    for (string word : allWords) {
+        if (distance == (word - refWord)) {// Overload minus operator
+            if (usedWords.contains(word)) continue;  // Avoid already used words
+            
+            nearbyWords.add(word);
+            usedWords.add(word);    // Avoid reuse in future via blacklist.
+        }
+    }
+}
+
+//
+// Function: getNLengthWords
+// Usage: getNLengthWords(length, lexicon, nLengthWords);
+// ------------------------------------------------------
+// Returns a reference variable lexicon of all words of specified length
+// found in another lexicon.
+//
+
+void getNLengthWords(int N, Lexicon & allWords, Lexicon & nLengthWords) {
+    
+    for (string word: allWords) {
+        if (N == word.length()) {
+            nLengthWords.add(word);
+        }
+    }
+}
