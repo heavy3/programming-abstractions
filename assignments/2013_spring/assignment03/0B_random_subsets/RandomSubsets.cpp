c56fbf162f9c653254cbb2056d19ae110c829032
zenglenn42 <gsnospampls@pobox.com>
2017-03-06 18:59:29 -0600

    oh, avoid iterative loops too

--- assignments/2013_spring/assignment03/0B_random_subsets/RandomSubsets.cpp
+++ assignments/2013_spring/assignment03/0B_random_subsets/RandomSubsets.cpp
@@ -2,6 +2,7 @@
 // RandomSubsets.cpp
 //
 // Given a set of integers, returns a uniformly-random subset of that set.
+// Avoids looping constructs when building the random subset.
 //
 // Example output:
 //
@@ -10,7 +11,7 @@
 //
 //     Displays a random subset of a set of integers.
 //
-//     A random subset of the set of integers from 0 to 99
+//     A random subset of the set of integers from 1 to 100
 //
 //     {2, 4, 5, 7, 9, 15, 16, 17, 24, 30, 33, 35, 41, 42, 43, 45,
 //     46, 50, 58, 59, 61, 66, 67, 68, 69, 73, 75, 77, 79, 82, 83,
@@ -43,6 +44,7 @@ const string DETAIL = "Random Subset";
 const string BANNER = HEADER + DETAIL;
 
 Set<int> randomSubsetOf(Set<int> & s);
+void recurRandomSubsetOf(Set<int> & subset, Set<int> & s);
 void seedMasterSet(Set<int> & s, int size);
 
 int main() {
@@ -54,7 +56,7 @@ int main() {
     seedMasterSet(s, SET_SIZE);
     Set<int> rs = randomSubsetOf(s);
     
-    cout << "A random subset of the set of integers from 0 to " << SET_SIZE -1;
+    cout << "A random subset of the set of integers from 1 to " << SET_SIZE;
     cout << endl << endl;
     
     cout << rs << endl;
@@ -62,19 +64,55 @@ int main() {
     return 0;
 }
 
+// Function: randomSubsetOf
+// Usage: Set<int> subset = randomSubsetOf(fullset);
+// ---------------------------------------------------
+// Returns a randomized subset of elements from the full set.
+//
+// Serves as a wrapper for a function which uses procedural recursion
+// to build the subset but at the expense of the full set which is
+// emptied in the process.
+//
+// The client remains blissfully unaware of the carnage since we operate
+// upon a copy of the passed-in set.
+
 Set<int> randomSubsetOf(Set<int> & s) {
     Set<int> subset;
+    Set<int> master(s);
     
-    for (int elem: s) {
-        if (randomChance(0.5)) {
-            subset.add(elem);
-        }
-    }
+    recurRandomSubsetOf(subset, master);
     return subset;
 }
 
-void seedMasterSet(Set<int> & s, int size) {
-    for (int i = 0; i < size; i++) {
-        s.add(i);
+// Function: recurRandomSubsetOf
+// Usage: recurRandomSubsetOf(subset, fullset);
+// --------------------------------------------
+// Uses recursion (instead of iteration) to populate a random selection
+// of elements from the second set into the first.
+
+void recurRandomSubsetOf(Set<int> & subset, Set<int> & fullset) {
+    
+    // base case: full set is empty
+    if (fullset.size() == 0) return;
+    
+    // recursive case
+    //
+    // reduce the size of the full set by one element
+    // flip a coin to decide if this element should be added to the subset
+    
+    int ri = fullset.first();
+    if (randomChance(0.5)) {
+        subset.add(ri);
     }
+    fullset.remove(ri);
+    
+    // perform the same operation on the n-1 sized fullset
+    
+    recurRandomSubsetOf(subset, fullset);
+}
+
+void seedMasterSet(Set<int> & s, int size) {
+    if (s.size() == SET_SIZE) return;
+    s.add(size);
+    seedMasterSet(s, --size);
 }
